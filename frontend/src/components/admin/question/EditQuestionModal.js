3dca82f0a641df1f2f0edc13c4a71025dbc7697e
Prajna Prasetya <prajnapras19@gmail.com>
2024-11-16 16:26:46 +0700

    feat: edit mcq options

--- frontend/src/components/admin/question/EditQuestionModal.js
+++ frontend/src/components/admin/question/EditQuestionModal.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { Modal, Button } from "react-bootstrap";
+import { Form, Modal, Button } from "react-bootstrap";
 import EditorJS from "@editorjs/editorjs";
 import Paragraph from "@editorjs/paragraph";
 import Underline from "@editorjs/underline";
@@ -12,6 +12,7 @@ const EditQuestionModal = (props) => {
     const {show, onClose, questionId, auth} = props;
     const [loading, setLoading] = useState(false);
     const editorInstance = useRef(null);
+    const [mcqOptions, setMcqOptions] = useState([]);
 
     const initializeEditor = async () => {
       try {
@@ -43,7 +44,6 @@ const EditQuestionModal = (props) => {
           data: JSON.parse(response.data.data.data),
         });
       } catch (err) {
-        console.log('err', err);
         toast.error(`Gagal mendapatkan data soal. Silakan coba beberapa saat lagi.`, {
           position: "top-center",
           autoClose: 5000,
@@ -100,8 +100,83 @@ const EditQuestionModal = (props) => {
     useEffect(() => {
       if (show) {
         initializeEditor();
+        const fetchData = async () => {
+          try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/admin/mcq-options/question-id/${questionId}`,
+              {}, {
+                headers: {
+                  Authorization: `Bearer ${auth.token}`,
+                },
+              },
+            );
+            setMcqOptions(response.data.data);
+          } catch (err) {
+            toast.error(`Gagal mendapatkan data soal. Silakan coba beberapa saat lagi.`, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+            });
+            handleClose();
+          }
+        };
+        fetchData();
       }
     }, [show]);
+
+    const handleOnChangeMcqOptions = (e, idx) => {
+      const {name, value} = e.target;
+
+      if (name === 'point' && value === '') {
+        return;
+      }
+
+      const currentObject = mcqOptions[idx];
+
+      if (name === 'point') {
+        currentObject[name] = Number(value);
+      } else {
+        currentObject[name] = value;
+      }
+      
+      
+      setMcqOptions(
+        (prevArray) =>
+          prevArray.map((item, i) => (i === idx ? currentObject : item))
+      );
+    }
+
+    const handleSaveMcqOption = async (e, idx) => {
+      e.preventDefault();
+      
+      try {
+        await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/api/v1/admin/mcq-options/${mcqOptions[idx].id}`, mcqOptions[idx], {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        });
+  
+        toast.success('Pilihan jawaban berhasil diubah!', {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      } catch (err) {
+        toast.error(`Gagal mengubah pilihan jawaban, silakan coba beberapa saat lagi.`, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      }
+    }
 
     return (
       <Modal show={show} onHide={handleClose} size="lg" centered>
@@ -117,12 +192,43 @@ const EditQuestionModal = (props) => {
           </Button>
           <hr/>
           <h3>Pilihan Jawaban</h3>
-          <Button variant="primary" disabled={loading}>
+          <Button variant="primary">
             Tambah Pilihan Jawaban
           </Button>
+          <hr/>
+          {mcqOptions.map((mcqOption, i) => (
+            <>
+              <Form className="my-4" onSubmit={(e) => handleSaveMcqOption(e, i)}>
+                <Form.Group className="my-3" controlId="description" key="description">
+                  <Form.Label><b>Deskripsi</b></Form.Label>
+                  <Form.Control
+                    type='text'
+                    name='description'
+                    value={mcqOption.description}
+                    onChange={(e) => handleOnChangeMcqOptions(e, i)}
+                    autoComplete='off'
+                  />
+                </Form.Group>
+                <Form.Group className="my-3" controlId="point" key="point">
+                  <Form.Label><b>Poin</b></Form.Label>
+                  <Form.Control
+                    type='number'
+                    name='point'
+                    step='1'
+                    value={mcqOption.point}
+                    onChange={(e) => handleOnChangeMcqOptions(e, i)}
+                    autoComplete='off'
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="me-3">Simpan</Button>
+                <Button variant="danger">Hapus</Button>
+              </Form>
+              <hr/>
+            </>
+          ))}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose} disabled={loading}>
+          <Button variant="secondary" onClick={handleClose}>
             Batal
           </Button>
         </Modal.Footer>
