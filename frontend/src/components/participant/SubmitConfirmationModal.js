463a468832fdbcc4e915deaa31f8a1c510657a10
Prajna Prasetya <prajnapras19@gmail.com>
2024-11-19 21:35:21 +0700

    fix copy

--- frontend/src/components/participant/SubmitConfirmationModal.js
+++ frontend/src/components/participant/SubmitConfirmationModal.js
@@ -5,9 +5,9 @@ const SubmitConfirmationModal = ({ show, handleClose, handleSubmit }) => {
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Konfirmasi Penghapusan</Modal.Title>
+        <Modal.Title>Konfirmasi Pengumpulan</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Apakah Anda yakin ingin mengumpulkan ujian?</Modal.Body>
+      <Modal.Body>Apakah Anda yakin ingin mengumpulkan ujian? Anda tidak akan bisa mengubah jawaban setelah dikumpulkan.</Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Tidak
