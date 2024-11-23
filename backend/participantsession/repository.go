e6b9ee16e048a420291c0acd2c3b1f46c8773680
Prajna Prasetya <prajnapras19@gmail.com>
2024-11-23 20:02:37 +0700

    feat: start exam on authorize session

--- backend/participantsession/repository.go
+++ backend/participantsession/repository.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/prajnapras19/project-form-exam-sman2/backend/config"
 	"github.com/prajnapras19/project-form-exam-sman2/backend/lib"
@@ -95,7 +96,13 @@ func (r *repository) AuthorizeSession(serial string) error {
 	r.cache.Del(context.Background(), r.GetParticipantSessionBySerialCacheKey(currentData.Serial))
 	r.cache.Del(context.Background(), r.GetLatestAuthorizedParticipantSessionByParticipantIDCacheKey(currentData.ParticipantID))
 
-	return r.db.Model(&ParticipantSession{}).Update("is_authorized", true).Error
+	return r.db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Model(&ParticipantSession{}).Update("is_authorized", true).Error
+		if err != nil {
+			return err
+		}
+		return tx.Table("participants").Where("id = ?", currentData.ParticipantID).Update("started_at", time.Now()).Error
+	})
 }
 
 func (r *repository) GetParticipantSessionBySerialCacheKey(serial string) string {
