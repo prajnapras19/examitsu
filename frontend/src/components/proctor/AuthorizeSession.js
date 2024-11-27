c8320c270cac48782c4e561ca20298fd0ffa2983
Prajna Prasetya <prajnapras19@gmail.com>
2024-11-27 16:14:21 +0700

    fix: select camera

--- frontend/src/components/proctor/AuthorizeSession.js
+++ frontend/src/components/proctor/AuthorizeSession.js
@@ -11,6 +11,35 @@ const AuthorizeSession = (props) => {
   const [scanning, setScanning] = useState(false);
   const [scannedData, setScannedData] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [state, setState] = useState({ cameraId: undefined, delay: 500, devices: [], loading: false })
+
+  navigator.mediaDevices.enumerateDevices()
+  .then((devices) => {
+    const videoSelect = []
+    devices.forEach((device) => {
+      if (device.kind === 'videoinput') {
+        videoSelect.push(device)
+      }
+    })
+    return videoSelect
+  })
+  .then((devices) => {
+    setState({
+      cameraId: devices[0].deviceId,
+      devices,
+      loading: false,
+    })
+  })
+  .catch((error) => {
+    toast.error(`Terjadi kesalahan.`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  });
 
   const handleCloseModal = () => {
     setShowModal(false);
@@ -31,6 +60,9 @@ const AuthorizeSession = (props) => {
   const resumeScanning = () => {
     setScanning(true);
     setScannedData("");
+    setState({
+      loading: true,
+    })
   };
   
   useEffect(() => {
@@ -51,6 +83,8 @@ const AuthorizeSession = (props) => {
     );
   }
 
+
+  const { loading, cameraId, devices } = state;
   return (
     <Container>
       <h1 className="my-4">Izinkan Ujian</h1>
@@ -59,16 +93,47 @@ const AuthorizeSession = (props) => {
       <Container className='mt-5 text-center'>
         {scanning ? (
           <Container>
-            <Container>
-              <Button variant='danger' onClick={pauseScanning}>Hentikan Pemindaian</Button>
-            </Container>
-            <Container className='mt-5'>
-              <QrScanner
-                delay={300}
-                style={{ width: "100%" }}
-                onScan={handleScan}
-              />
-            </Container>
+            {state.loading
+            ? (
+              <Container className="text-center">
+                <Spinner animation="border" />
+                <p>Mohon tunggu...</p>
+              </Container>
+            )
+            : (
+                <>
+                  <Container>
+                    <Button variant='danger' onClick={pauseScanning}>Hentikan Pemindaian</Button>
+                  </Container>
+                  <Container className="mt-5 flex">
+                    <Container>
+                      Kamera:
+                    </Container>
+                    <Container>
+                      <select
+                        onChange={e => {
+                          const value = e.target.value
+                          this.setState({ cameraId: undefined }, () => {
+                            this.setState({ cameraId: value })
+                          })
+                        }}
+                      >
+                        {devices.map((deviceInfo, index) => (
+                          <React.Fragment key={deviceInfo.deviceId}><option value={deviceInfo.deviceId}>{deviceInfo.label || `camera ${index}`}</option></React.Fragment>
+                        ))}
+                      </select>
+                    </Container>
+                  </Container>
+                  <Container className='mt-5'>
+                    <QrScanner
+                      delay={300}
+                      style={{ width: "100%" }}
+                      onScan={handleScan}
+                    />
+                  </Container>
+                </>
+              )
+            }
           </Container>
         ) : (
           <Container>
